06e17184d9d2dc6975d810518137f46855e56710
James David Clarke <james@jamesdavidclarke.com>
2023-10-01 15:39:27 +0100

    Made View Details button blue

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -24,7 +24,7 @@ export default function Grid(grid_props: GridProps) {
           <p class="mt-2 mb-2 text-center">
             {grid.description}
           </p>
-          <p class="mt-2 mb-2">
+          <p class="mt-2 mb-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
             <a href={grid.url}>
               View details
             </a>
